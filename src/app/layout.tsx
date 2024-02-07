77111c55611850c3eb1fbafd9218979d19458eb7
kuchaidev <kristntan@gmail.com>
2024-02-07 22:57:35 +0800

    Add sign in, sign up,  Clerk  authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +15,25 @@ export const metadata: Metadata = {
   title: "Notesai",
   description: "Notion-like",
 };
+
+
+ 
+function Header() {
+  return (
+    <header style={{ display: "flex", justifyContent: "space-between", padding: 20 }}>
+      <h1>Notesai</h1>
+      <SignedIn>
+        {/* Mount the UserButton component */}
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        {/* Signed out users get sign in button */}
+        <SignInButton/>
+      </SignedOut>
+    </header>
+  );
+}
+ 
 
 export default function RootLayout({
   children,
@@ -18,7 +43,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}><Header />{children}</body>
     </html>
     </ClerkProvider>
   );
