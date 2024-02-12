85c53901ce0c4de8477247b4041610839e10ccc8
kuchaidev <kristntan@gmail.com>
2024-02-12 17:17:58 +0800

    Add Drizzle ORM database

---
+++ src/lib/db/schema.ts
@@ -0,0 +1,14 @@
+// Postgres core
+import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+
+// table name 'notes'
+export const $notes = pgTable('notes', {
+    id: serial('id').primaryKey(),
+    name: text('name').notNull(),
+    createdAt: timestamp('created_at').notNull().defaultNow(),
+    imageUrl: text('imageUrl'),
+    userId: text('user_id').notNull(),
+    editorState: text('editor_state'),
+});
+
+export type NoteType = typeof $notes.$inferInsert;
